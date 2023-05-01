3bb682bb3165895509ad7e4b09d85d21e4d39e23
Joshua Ondieki <oj@joshuaondieki.com>
2023-05-01 18:52:45 +0300

    Generate coverage reports

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,6 @@
 json-server --watch testdb.json --port 3000 &
 server_pid=$!
-vitest --run --reporter verbose
+vitest --run --reporter verbose --coverage ./coverage/coverage.json
 
 kill $server_pid
 
