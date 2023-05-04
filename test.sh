64bbc963aca8e35bdde63dbebc020effe3a0758e
Joshua Ondieki <oj@joshuaondieki.com>
2023-05-04 17:41:26 +0300

    Update test script setup

--- test.sh
+++ test.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 json-server --watch tests/testdb.json --port 3000 &
 server_pid=$!
 
 # vitest --reporter verbose --coverage ./coverage/coverage.json --coverage-provider istanbul
-vitest run --coverage **/*.js --config ./vitest.config.js
+vitest run --coverage **/*.js --config ./vitest.config.ts
 
 kill $server_pid
 
