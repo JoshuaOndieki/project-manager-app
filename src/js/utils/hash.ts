39241f40196462f954b35ca800e20c5cce23c1b7
Joshua Ondieki <oj@joshuaondieki.com>
2023-05-07 16:28:58 +0300

    Remove unnecessary imports causing errors

--- src/js/utils/hash.ts
+++ src/js/utils/hash.ts
@@ -1,9 +1,8 @@
-// const crypto = window.crypto || window.msCrypto;
 
-// // Use built-in crypto to generate a unique ID
-// function generateUUID() {
-//     const array = new Uint32Array(4);
-//     crypto.getRandomValues(array);
-//     const uuid = array.join('-');
-//     return uuid;
-// }
+// Use built-in crypto to generate a unique ID
+export function generateUUID() {
+    const array = new Uint32Array(4);
+    crypto.getRandomValues(array);
+    const uuid = array.join('-');
+    return uuid;
+}
